c21ce35c59c3dd497ccb75d23819826a7ed13cfb
Wei Rong Yap <kenny@twoapp.studio>
2022-05-09 15:48:36 +0800

    Build out folder

--- out/src/latex/latex_parser_simple.js
+++ out/src/latex/latex_parser_simple.js
@@ -5495,15 +5495,9 @@ function peg$parse(input, options) {
       if (s2 !== peg$FAILED) {
         s3 = [];
         s4 = peg$parseArgumentList();
-        if (s4 === peg$FAILED) {
-          s4 = peg$parseGroup();
-        }
         while (s4 !== peg$FAILED) {
           s3.push(s4);
           s4 = peg$parseArgumentList();
-          if (s4 === peg$FAILED) {
-            s4 = peg$parseGroup();
-          }
         }
         s4 = peg$parseskip_space();
         s5 = [];
